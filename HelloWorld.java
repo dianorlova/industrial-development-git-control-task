a4c78061674a1eea8decd44becbaf2badfa35494
dianorlova <dianaorlova0101@gmail.com>
2021-09-16 10:27:28 +0300

    IV.5

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello World!1");
-=======
-        System.out.println("Hello World!\n");
->>>>>>> adb7228 (Add \n)
+        System.out.println("Hello World!1\n");
 	System.out.println("New Feature!");
     }
 }
